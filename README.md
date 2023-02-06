0f19fd9573760fe6e149d24b334846fc7f29d817
YoungKwang Nam <coolbirds@naver.com>
2023-02-06 15:17:30 +0900

    [20230206] First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+[20230226] ReaderWriterLock 구현 연습
---
+++ ServerCore/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+
+    internal class Program
+    {
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
+
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count++;
+                    _lock.WriteUnlock();
+                }
+            });
+
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnlock();
+                }
+            });
+
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(count);
+        }
+    }
+}
