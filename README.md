e36a23a760a66d887e06cbb18fc197b3b1b994d1
YoungKwang Nam <coolbirds@naver.com>
2023-02-08 16:27:44 +0900

    [20230208] sender async

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [20230206] Thread Local Storage(TLS) 구현 연습  
 [20230207] Onblocking, 즉 Async비동기 방식 연습  
 [20230208] Session작성, Receive를 비동기로.  
+[20230208] Session작성, send를 비동기로.  
--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -12,19 +12,31 @@ namespace ServerCore
         Socket _socket;
         int _disconnected = 0; // 끊겼는지 여부를 관리.
 
+        object _lock = new object();
+        Queue<byte[]> _sendQueue= new Queue<byte[]>();
+        bool _pending = false;
+        SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+
         public void Start(Socket socket)
         {
             _socket= socket;
             SocketAsyncEventArgs recvArgs = new SocketAsyncEventArgs();
             recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
             recvArgs.SetBuffer(new byte[1024], 0, 1024);//0번부터 사이즈1024인 버퍼를 할당.
+            
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
 
             RegisterRecv(recvArgs);            
         }
 
         public void Send(byte[] sendBuff)
         {
-            _socket.Send(sendBuff);
+            lock (_lock)
+            {
+                _sendQueue.Enqueue(sendBuff);
+                if (_pending == false)
+                    RegisterSend();
+            }
         }
 
         public void Disconnect()
@@ -37,6 +49,43 @@ namespace ServerCore
         }
 
         #region 네트워크 통신
+
+        void RegisterSend()
+        {
+            _pending = true;
+            byte[] buff = _sendQueue.Dequeue();
+            _sendArgs.SetBuffer(buff, 0, buff.Length);
+
+            bool pending = _socket.SendAsync(_sendArgs);
+            if (pending == false)
+                OnSendCompleted(null, _sendArgs);
+        }
+
+        void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            lock (_lock)
+            {
+                if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+                {
+                    try
+                    {
+                        if (_sendQueue.Count > 0)
+                            RegisterSend();
+                        else
+                            _pending = false;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"OnSendCompleted Failed {e}");
+                    }
+                }
+                else
+                {
+                    Disconnect();
+                }
+            }
+        }
+
         void RegisterRecv(SocketAsyncEventArgs args)
         {
             bool pending = _socket.ReceiveAsync(args); //pending: 보류중
@@ -65,6 +114,7 @@ namespace ServerCore
             {
 
                 // TODO Disconnect
+                Disconnect();
             }
         }
 
